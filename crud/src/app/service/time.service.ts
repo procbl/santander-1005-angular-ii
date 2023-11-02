dcd7c84890ee25fe2f3efbd96558cfd832c67737
Cleber <cleberdeoliveira.ti@gmail.com>
2023-11-01 21:12:52 -0300

    atualizando o db e criando service de time

---
+++ crud/src/app/service/time.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TimeService {
+  
+
+  constructor(private http: HttpClient) {}
+
+  getTimes(): Observable<any> {
+    return this.http.get('http://localhost:3000/times')
+  }
+
+  criarTime(time: string): Observable<any>  {
+    return this.http.post('http://localhost:3000/times', time)
+  }
+
+  editarTime(time: string): Observable<any>  {
+    return this.http.put(`http://localhost:3000/times/${time.id}`, time)
+  }
+
+  excluirTime(time: string): Observable<any>  {
+    return this.http.delete(`http://localhost:3000/times/${time}`)
+  }
+
+}
